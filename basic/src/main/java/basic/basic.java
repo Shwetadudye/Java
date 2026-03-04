c7342f28700c02d60e334dfb3da67ae81bffbde1
Shweta Dudye <dudyeshweta@gmail.com>
2026-03-04 20:28:44 +0530

    Basics of java

---
+++ basic/src/main/java/basic/basic.java
@@ -0,0 +1,89 @@
+package basic;
+//import java.util.Scanner;
+//
+//public class basic {
+////
+////	public static void main(String[] args) {
+////		Scanner sc = new Scanner(System.in);
+////		System.out.print("Enter your Name :");
+////		String name = sc.nextLine();
+////		System.out.println("Welcome " + name);
+////
+////	}
+
+
+//public static void main(String[] args) {
+//int  marks = 40;
+//String name = "Shweta";
+//double price = 99.50;
+//char grade = 'A';
+//	System.out.println(grade);
+//}
+//}
+//
+
+//   public class basic {
+//	   
+//	   public static void main(String[] args) {
+//	   int a = 10 ;
+//	    Integer obj = Integer.valueOf(a);
+//	    System.out.println(obj);
+//	    
+//	    Integer b = 20;
+//	    int c = b.intValue();
+//	    System.out.println(c);
+//	   }
+//   }
+
+
+//  Conditional statement 
+//
+//      public class basic {
+//    	  
+//    	  public static void main(String[] args) {
+//    		  int a = 3;
+//    		  int b = 3;
+//    		  
+//    		  if (a>b) {
+//    			  System.out.println("a is greater than b"); 
+//    	  }else if (a<b) {
+//    		  System.out.println("b is greater");
+//    	  }else {
+//    		  System.out.println("Both are equal");
+//    	  }
+//      }
+//    	 
+//      }
+
+
+// For loop 
+
+//   
+
+
+   // Arithmatic operators  + , - ,* ,/, %
+
+     public class basic {
+    	public static void main(String[] args) {
+    	int price = 250;
+    	int tickets = 3;
+    	
+    	
+    	int total = price* tickets;
+    	int add = price+ tickets;
+    	int sub = price - tickets;
+    	int div = price / tickets;
+    	int mod = price % tickets;
+    	
+    	
+    	
+    	System.out.println("Total of your ticket "+ total);
+    	System.out.println("Addition " + add);
+    	System.out.println("Sub " + sub);
+    	System.out.println("div " + div);
+    	System.out.println("mod " + mod);
+    	
+    	}
+    	
+     }
+     
