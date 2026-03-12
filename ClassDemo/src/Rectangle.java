45b00204f3edc3133677ccbe82aae47378890613
Shweta Dudye <dudyeshweta@gmail.com>
2026-03-12 17:50:57 +0530

    Added java code

---
+++ ClassDemo/src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+
+    public static void  main (String[] args){
+        int length = 45;
+        int width = 3;
+
+        int Area = length * width ;
+
+        System.out.println("Area of rectangle: " + Area);
+
+        int s = 5;
+
+        int A = s*s ;
+
+        System.out.println("Square: " + A);
+    }
+}
