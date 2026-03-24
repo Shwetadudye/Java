1a0e9a9bb673e6805f94e033699a9dabe0756da1
Shweta Dudye <dudyeshweta@gmail.com>
2026-03-24 13:49:03 +0530

    switch case

---
+++ JavaBasics/src/Switchcase.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Switchcase {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Operator");
+
+        char operator = sc.next().charAt(0);
+
+        switch (operator){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                System.out.println(a/b);
+        }
+
+
+    }
+}
