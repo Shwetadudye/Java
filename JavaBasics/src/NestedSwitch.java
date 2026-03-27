5bf27d19e26b226672cb8f6a2a67d339094807b0
Shweta Dudye <dudyeshweta@gmail.com>
2026-03-27 12:36:22 +0530

    Code of the day

--- JavaBasics/src/NestedSwitch.java
+++ JavaBasics/src/NestedSwitch.java
@@ -7,28 +7,17 @@ public class NestedSwitch {
         System.out.println("Please Enter Your department ");
         String Department = in.next();
 
-        switch (EmployeeID){
-            case 1:
-                System.out.println("Hello Omkar");
-                break;
-            case  2 :
-                System.out.println("Hello Shweta");
-                break;
-            case 3 :
-                switch (Department){
-                    case "It":
-                        System.out.println("It Department");
-                        break;
-                    case "Manager":
-                        System.out.println("Management Department");
-                        break;
-                    default:
-                        System.out.println("Invalid Your department");
+        switch (EmployeeID) {
+            case 1 -> System.out.println("Hello Omkar");
+            case 2 -> System.out.println("Hello Shweta");
+            case 3 -> {
+                switch (Department) {
+                    case "It" -> System.out.println("It Department");
+                    case "Manager" -> System.out.println("Management Department");
+                    default -> System.out.println("Invalid Your department");
                 }
-                break;
-            default:
-                System.out.println("Invalid Id");
-
+            }
+            default -> System.out.println("Invalid Id");
         }
     }
 }
