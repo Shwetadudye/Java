3cd8b48c30e689c9e10a41950563c1b174532d92
Shweta Dudye <dudyeshweta@gmail.com>
2026-03-29 20:30:10 +0530

    Strings

--- Functins and methods/src/Stringfunc.java
+++ Functins and methods/src/Stringfunc.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Stringfunc {
     public static void main(String[] args){
+//        String message = greet();
+//        System.out.println(message);
+        String notes = greet("shweta");
+        System.out.println(notes);
+    }
+//    static  String greet(){
+//        String greeting = "How are u";
+//        return  greeting;
+//    }
 
+    static  String greet(String name){
+        String msg = "hello" + name;
+        return msg;
     }
 }
