6134d8f99bf541fab111597ca830c02158ab1736
Shweta Dudye <dudyeshweta@gmail.com>
2026-04-02 14:48:05 +0530

    Stream API

--- Java oops/src/Streamapi.java
+++ Java oops/src/Streamapi.java
@@ -1,10 +1,13 @@
+import java.sql.ClientInfoStatus;
+import java.util.Collection;
 import java.util.List;
 import java.util.Arrays;
 import java.util.stream.*;
 
 public class Streamapi {
     public static void main(String[] args){
-        List <Integer> list = Arrays.asList(23,10,30,6,12,20); //   12,20,23,30
+//        List <Integer> list = Arrays.asList(23,10,30,6,12,20); //   12,20,23,30
+        List <Integer> list = Arrays.asList(1,2,6,4,5);
 
       //   list.stream().forEach(System.out :: println );
         // filter method
@@ -16,8 +19,19 @@ public class Streamapi {
 //                      .map(n->n*n)
 //                      .forEach(System.out :: println);
          // reduce method
-        int sum = list.stream().reduce(0,(a,b)-> a+b);
-        System.out.println("sum : " + sum);
+//        int sum = list.stream().reduce(0,(a,b)-> a+b);
+//        System.out.println("sum : " + sum);
+
+        list.stream().peek(n->System.out.println("before" + n))
+                     .map(n->n*2).peek(n->System.out.println("After" + n))
+                     .collect(Collectors.toList());
+
+        long count = list.stream().count();
+        System.out.println("Count" + count);
+
+        int sum = list.stream().mapToInt(n->n).sum();
+        System.out.println(sum);
+
 
     }
 }
